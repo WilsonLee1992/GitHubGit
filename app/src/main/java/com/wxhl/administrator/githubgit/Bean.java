021819d7aa807dd96195e65c1a2a12d7ed02eafa
WilsonLee1992 <274229518@qq.com>
2016-12-27 17:31:10 +0800

    bean------

---
+++ app/src/main/java/com/wxhl/administrator/githubgit/Bean.java
@@ -0,0 +1,21 @@
+package com.wxhl.administrator.githubgit;
+
+/**
+ * Created by Administrator on 2016/12/27.
+ */
+public class Bean {
+    private int num;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
