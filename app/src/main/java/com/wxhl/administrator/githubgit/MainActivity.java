6b4d93a568f5053598a781912e58d37d608bf419
WilsonLee1992 <274229518@qq.com>
2016-12-27 17:25:40 +0800

    啦啦啦诺克萨斯

--- app/src/main/java/com/wxhl/administrator/githubgit/MainActivity.java
+++ app/src/main/java/com/wxhl/administrator/githubgit/MainActivity.java
@@ -7,5 +7,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
